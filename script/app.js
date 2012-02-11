3ab8ef4e211678a8f17166554b2976dfbbb7f60a
Isak Wiström <isak.wistrom@gmail.com>
2012-02-11 21:51:26 +0100

    removed logging

--- script/app.js
+++ script/app.js
@@ -24,7 +24,7 @@ $(window).load( function(){
     scroller = new iScroll('content_id', { 
         hScrollbar: false, 
         vScrollbar: true,
-        onScrollStart : function(){
+        onScrollStart : function(){;
             allowedToSnap = false;
         },
         
